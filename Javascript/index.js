0dfb55cfa5123a390237488d7da74f451d316208
Enis Hamza <eniss.hamza@gmail.com>
2025-01-11 17:54:19 +0100

    Javascript Data Types

---
+++ Javascript/index.js
@@ -0,0 +1,13 @@
+console.log("Hello World");
+
+const name = "Enis Hamza";
+const number = 23;
+const gender = "male";
+const male = true;
+let date = new Date();
+
+const full = name + " " + number;
+console.log(full);
+console.log(gender);
+console.log(male);
+console.log(date);
