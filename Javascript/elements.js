44258d0afda52be7018e05d3a63c2383c3dd0505
Enis Hamza <eniss.hamza@gmail.com>
2025-02-13 15:40:17 +0100

    Styling with Javascript

--- Javascript/elements.js
+++ Javascript/elements.js
@@ -1,6 +1,20 @@
+//CreateElement and append method
 const container = document.querySelector(".clickMe");
 const butoni = document.createElement("button");
 console.log(butoni);
 const shkruaj = document.createElement("input");
 
 container.append(butoni, shkruaj);
+
+//Styling with Javascript
+const ndrysho = document.querySelector("h1");
+ndrysho.classList.add("red-color");
+
+function toggle() {
+  ndrysho.classList.toggle("green-color");
+}
+document.addEventListener("click", toggle);
+
+const body = document.querySelector("body");
+body.style.backgroundColor = "bisque";
+ndrysho.style.textAlign = "center";
