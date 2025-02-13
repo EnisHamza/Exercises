ee4633f555656953a267ced9ee2be3450ed7f8c6
Enis Hamza <eniss.hamza@gmail.com>
2025-02-13 11:54:26 +0100

    Create Element and append methods

---
+++ Javascript/elements.js
@@ -0,0 +1,6 @@
+const container = document.querySelector(".clickMe");
+const butoni = document.createElement("button");
+console.log(butoni);
+const shkruaj = document.createElement("input");
+
+container.append(butoni, shkruaj);
