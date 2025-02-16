60bf6025e304d3ad329307ff4d5bb42a5b78f5f3
Enis Hamza <eniss.hamza@gmail.com>
2025-02-16 20:16:50 +0100

    Working with numbers

--- Javascript/elements.js
+++ Javascript/elements.js
@@ -18,3 +18,28 @@ document.addEventListener("click", toggle);
 const body = document.querySelector("body");
 body.style.backgroundColor = "bisque";
 ndrysho.style.textAlign = "center";
+
+//Working with numbers
+console.log("123" === 123);
+console.log(Number("123") === 123);
+
+const revenue = 13456.7372;
+const formated = revenue.toFixed(3);
+console.log(formated);
+
+const budget2024 = 432.232344;
+const budget2025 = 485.261272;
+function shuma() {
+  const total = budget2024 + budget2025;
+  return Number(total.toFixed(2));
+}
+console.log(shuma());
+
+const drinks = 342.4523;
+const meals = 765.2826;
+function getTotal() {
+  const sum = drinks + meals;
+  const calculate = (sum / 100) * 12 + sum;
+  return Number(calculate.toFixed(2));
+}
+console.log(getTotal());
