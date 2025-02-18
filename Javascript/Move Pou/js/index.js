791a4f43431f239a0be827332fe244606ce9bf70
Enis Hamza <eniss.hamza@gmail.com>
2025-02-18 18:35:08 +0100

    Moving pou project

---
+++ Javascript/Move Pou/js/index.js
@@ -0,0 +1,31 @@
+const pou = document.querySelector(".bob");
+const eyes = document.querySelector(".eye-container");
+
+let left = 100;
+
+function moveRight() {
+  left += 100;
+  pou.style.left = left + "px";
+  console.log(pou.style.left);
+  eyes.style.transform = "rotate(0deg)";
+  eyes.style.left = "4.5rem";
+}
+
+function moveLeft() {
+  left -= 100;
+  pou.style.left = left + "px";
+  console.log(pou.style.left);
+  eyes.style.transform = "rotate(180deg)";
+  eyes.style.left = "-4.5rem";
+}
+
+function moveBob(e) {
+  console.log("Pressing key", e.key);
+  if (e.key === "ArrowLeft") {
+    moveLeft();
+  }
+  if (e.key === "ArrowRight") {
+    moveRight();
+  }
+}
+document.addEventListener("keydown", (e) => moveBob(e));
