79e359d47ca123b985fefff90ef4a322c22904b1
Enis Hamza <eniss.hamza@gmail.com>
2025-02-25 18:39:02 +0100

    Drag events

--- Javascript/events.js
+++ Javascript/events.js
@@ -6,3 +6,21 @@ function keydown(e) {
   console.log(e.key);
 }
 document.addEventListener("keydown", (e) => keydown(e));
+
+//Drag events
+const homeElement = document.querySelector("#home");
+const chickElement = document.querySelector("#chick");
+
+chickElement.addEventListener("drag", (e) => console.log("dragging"));
+chickElement.addEventListener("dragstart", (e) => console.log("start"));
+chickElement.addEventListener("dragend", (e) => console.log("end"));
+homeElement.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  console.log("over");
+});
+homeElement.addEventListener("dragenter", (e) => console.log("enter"));
+homeElement.addEventListener("dragleave", (e) => console.log("leave"));
+homeElement.addEventListener("drop", (e) => {
+  console.log("drop");
+  homeElement.append(chickElement);
+});
