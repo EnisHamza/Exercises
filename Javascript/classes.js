702daf16e798cea13adcdbc13dc37f3f709ad506
Enis Hamza <eniss.hamza@gmail.com>
2025-03-04 21:49:37 +0100

    Javascript select dropdown

--- Javascript/classes.js
+++ Javascript/classes.js
@@ -10,3 +10,15 @@ const greenShape = new Shape(200, 100, 0, "green");
 const redShape = new Shape(80, 80, 40, "red");
 
 console.log(greenShape);
+
+const selectDropdown = document.querySelector("#flavor-select");
+const submitButton = document.querySelector("input[type=submit]");
+
+selectDropdown.value;
+
+function handleClick(e) {
+  e.preventDefault();
+  console.log(selectDropdown.value);
+}
+
+submitButton.addEventListener("click", handleClick);
