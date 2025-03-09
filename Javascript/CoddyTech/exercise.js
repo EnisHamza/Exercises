b8d9db95958271378951d5409ef2e1bea8177b8c
Enis Hamza <eniss.hamza@gmail.com>
2025-03-09 18:22:35 +0100

    Exercise with loops

--- Javascript/CoddyTech/exercise.js
+++ Javascript/CoddyTech/exercise.js
@@ -22,3 +22,23 @@ for (let i = 1; i <= 20; i++) {
   }
   console.log(i);
 }
+
+//Write a program that finds all pairs of numbers that add up to n using numbers from 1 to n - 1.
+let n = 7;
+for (let i = 1; i < n; i++) {
+  for (let j = 1; j < n; j++) {
+    if (i + j == n) {
+      console.log(i, j);
+    }
+  }
+}
+
+//Write a do...while loop that starts with a variable count set to 10. The loop should:
+/*Print "Counting down: [count]", where [count] is the current value of count.
+Decrease count by 2 after each iteration.
+Stop when count becomes less than 0.*/
+let count = 10;
+do {
+  console.log("Counting down: " + count);
+  count -= 2;
+} while (count >= 0);
