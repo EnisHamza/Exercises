2a19b62ae65eb9af9a28677cc706a82e0625efc7
Enis Hamza <eniss.hamza@gmail.com>
2025-03-13 16:11:02 +0100

    Array methods challenges

--- Javascript/CoddyTech/exercise.js
+++ Javascript/CoddyTech/exercise.js
@@ -246,3 +246,152 @@ function firstAndLast(arr) {
   return arr[0] + arr[arr.length - 1];
 }
 console.log(firstAndLast([1, 2, 3, 4, 17]));
+
+//Create a function named findElement that receives two arguments:
+/*An array of numbers
+A number to search for
+The function should do the following:
+Check if the array includes the number using the includes method.
+If the number is found, return the index of its first occurrence using the indexOf method.
+If the number is not found, return -1.*/
+function findElement(arr, num) {
+  let isPart = arr.includes(num);
+  if (isPart === true) {
+    return arr.indexOf(num);
+  } else {
+    return arr.indexOf(num);
+  }
+}
+console.log(findElement([1, 2, 3, 4], 262348));
+
+//Write a function named processArray that takes an array of numbers as an argument. The function should do the following:
+/*Remove the last element from the array.
+Add the number 10 to the end of the array.
+Sort the array.
+Check if the array includes the number 5. If it does, replace it with the number 50.
+Reverse the order of the elements in the array.
+Return the modified array.*/
+function processArray(array) {
+  array.pop();
+  array.push(10);
+  array.sort();
+
+  let isPart = array.includes(5);
+  if (isPart == true) {
+    let index = array.indexOf(5);
+    let duplicate = (array[index] = 50);
+  }
+
+  array.reverse();
+  return array;
+}
+console.log(processArray([9, 6, 3, 12]));
+
+//Create a program that receives an array of strings as input (given),
+//and prints a new array containing only the words longer than 5 characters.
+let arr = ["Kosova", "Enis", "Shqiperia"];
+let rezultati = [];
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i].length > 5) {
+    rezultati.push(arr[i]);
+  }
+}
+console.log(rezultati);
+
+//Write a program that receives an array of numbers as input (given),
+//and prints an array of the numbers that are either below 50 or are divisible by 5. Use the forEach method.
+let arra = [1, 2, 3, 4, 5, 25, 50, 75, 100];
+let resulta = [];
+arra.forEach((arr) => {
+  if (arr < 50 || arr % 5 === 0) {
+    resulta.push(arr);
+  }
+});
+console.log(resulta);
+
+//Write a function named countVowels that takes a string as an argument and returns
+//the number of vowels (a, e, i, o, u) in the string.
+//Use a for...of loop to iterate over the characters of the string.
+function countVowels(str) {
+  let count = 0;
+  let vowels = "aeiouAEIOU";
+  for (let char of str) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(countVowels("Hello Worlds"));
+
+//Create a program that receives a string as input (given), and it prints how many times the character p is in the string.
+//Some chars might be upper cased, use char.toLowerCase() to convert it to lower cased.
+let text = "Pampirinc";
+text = text.toLowerCase();
+let counti = 0;
+let letter = "p";
+for (let char of text) {
+  if (letter.includes(char)) {
+    counti++;
+  }
+}
+console.log(counti);
+
+//Write a function named countDigits that takes a string as an argument and returns the number of digits (0-9) in the string.
+//Use a for...of loop to iterate over the characters of the string.
+function countDigits(str) {
+  let count = 0;
+  let numbers = "1234567890";
+  for (let char of str) {
+    if (numbers.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(countDigits("viti209232824"));
+
+/*Perform the following tasks in order:
+
+Sort the array in ascending order using a method.
+Reverse the order of the array using a method.
+Find the index of the first occurrence of the number 30 and store it in a variable named firstIndex.
+Find the index of the last occurrence of the number 10 and store it in a variable named lastIndex.
+Check if the number 100 is included in the array and store the result in a variable named hasHundred.
+Finally, print the following:
+
+The updated array after sorting and reversing.
+The value of firstIndex.
+The value of lastIndex.
+The value of hasHundred. */
+let numbers = [40, 10, 30, 20, 50, 30, 10];
+numbers.sort().reverse();
+let firstIndex = numbers.indexOf(30);
+let lastIndex = numbers.lastIndexOf(10);
+let hasHundred = numbers.includes(100);
+
+console.log(numbers);
+console.log(firstIndex);
+console.log(lastIndex);
+console.log(hasHundred);
+
+/*Your task is to perform the following operations in order:
+
+Add "yellow" to the end of the array using a method.
+Remove the first color from the array and store it in a variable named removedColor.
+Add "purple" to the beginning of the array using a method.
+Remove the last color from the array and store it in a variable named lastColor.
+Finally, print the following:
+
+The updated array.
+The value of removedColor.
+The value of lastColor. */
+let colors = ["red", "blue", "green"];
+colors.push("yellow");
+let removedColor = colors.shift();
+colors.unshift("purple");
+let lastColor = colors.pop();
+
+console.log(colors);
+console.log(removedColor);
+console.log(lastColor);
