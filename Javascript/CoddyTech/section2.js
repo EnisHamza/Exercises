c42a6b75e686a2e17a59ddd6f08a6f249fa21bff
Enis Hamza <eniss.hamza@gmail.com>
2025-03-25 23:34:07 +0100

    Another way to write a pyramid with js

--- Javascript/CoddyTech/section2.js
+++ Javascript/CoddyTech/section2.js
@@ -199,3 +199,33 @@ function pyramids(height) {
   }
 }
 pyramids(4);
+
+//Another way of building pyramids
+const character = "#";
+const count = 3;
+const rows = [];
+let inverted = true;
+
+function padRow(rowNumber, rowCount) {
+  return (
+    " ".repeat(rowCount - rowNumber) +
+    character.repeat(2 * rowNumber - 1) +
+    " ".repeat(rowCount - rowNumber)
+  );
+}
+
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
+
+let result = "";
+
+for (const row of rows) {
+  result = result + row + "\n";
+}
+
+console.log(result);
